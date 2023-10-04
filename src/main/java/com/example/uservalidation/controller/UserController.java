d57cf1a915296af0935c821d25104aef921dbe22
Endre Varga <142538289+evargaa@users.noreply.github.com>
2023-10-04 13:19:23 +0200

    Update UserController.java

--- src/main/java/com/example/uservalidation/controller/UserController.java
+++ src/main/java/com/example/uservalidation/controller/UserController.java
@@ -1,2 +1,28 @@
-package com.example.uservalidation.controller;public class UserController {
+package com.example.uservalidation.controller;
+
+
+import com.example.uservalidation.model.User;
+import com.example.uservalidation.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    }
+
 }
